2c213bef4363544e61ac8cc63c2d68327571a346
Vincent Manuceau <manuceauv@gmail.com>
2021-07-26 23:09:54 +0200

    linux / mac interop

--- ipfs_update_ports.sh
+++ ipfs_update_ports.sh
@@ -6,6 +6,7 @@ sed -e "s/\/ip4\/127.0.0.1\/tcp\/5001/\/ip4\/127.0.0.1\/tcp\/$1/g" \
 -e "s/\/ip4\/0.0.0.0\/udp\/4001\/quic/\/ip4\/0.0.0.0\/udp\/$3\/quic/g" \
 -e "s/\/ip6\/::\/udp\/4001\/quic/\/ip6\/::\/udp\/$3\/quic/g" \
 -e "s/\"Router\": \"\"/\"Router\": \"$5\"/g" \
--i .ipfs$4/config
+.ipfs$4/config > .ipfs$4/config1
+mv .ipfs$4/config1 .ipfs$4/config
 cp .ipfs/swarm.key .ipfs$4/
 
