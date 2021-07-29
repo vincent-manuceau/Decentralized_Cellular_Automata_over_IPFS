7b781bc914fa5cccf283ba0a6b94e2e256933005
Vincent Manuceau <manuceauv@gmail.com>
2021-07-29 14:29:38 +0200

    debug

--- generate_swarm_key.sh
+++ generate_swarm_key.sh
@@ -2,7 +2,7 @@
 
 
 if [ $(uname) = 'Linux' ]; then
- echo -e "/key/swarm/psk/1.0.0/\n/base16/\n`tr -dc 'a-f0-9' < /dev/urandom | head -c64`" > ./.ipfs/swarm.key
+ echo "/key/swarm/psk/1.0.0/\n/base16/\n`tr -dc 'a-f0-9' < /dev/urandom | head -c64`" > ./.ipfs/swarm.key
  sed -e "s/\"Router\": \"\"/\"Router\": \"$1\"/g" -i .ipfs/config
 elif [ $(uname) = 'Darwin' ]; then
  ./generate_swarm_key_mac > ./.ipfs/swarm.key
